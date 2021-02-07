b881c64caf74e71d956ba18d893707a8521b7ba7
toreapat <torea.patissier@laplateforme.io>
2021-02-07 15:58:21 +0100

    Push du dimanche

--- admin.php
+++ admin.php
@@ -1,15 +1,21 @@
 <?php
     session_start();
+
 require_once('html_partials/header.php');
+
+require_once('classes/admin.class.php');
+
 // Si l'user n'est pas un admin, on le redirige gentiment vers l'accueil
+
 if($id_droits != 1337 && $id_droits != 42){
     header('location:http://localhost:8888/blog/index.php');
 exit();
 }
-require_once('classes/admin.class.php');
 
 $ADMIN = new Admin();
+
 // $ADMIN->AdminGestion();
+
 $ADMIN->AdminGestion();
 
 
--- classes/admin.class.php
+++ classes/admin.class.php
@@ -6,22 +6,42 @@ class Admin extends bdd{
     public function AdminGestion()
     {
         $con = $this->connectDb();
-        $stmt = $con->prepare('SELECT * FROM articles INNER JOIN categories ON articles.id_categorie = categories.id ORDER BY categories.id');
+        $stmt = $con->prepare('SELECT * FROM articles INNER JOIN categories ON  articles.id_categorie = categories.id ORDER BY categories.id');
         $stmt2 = $con->prepare('SELECT nom FROM categories');
         $stmt->execute();
         $stmt2->execute();
         $result = $stmt->fetchAll();
         $result2 = $stmt2->fetchAll();
+
+        echo'<pre>';
+        // var_dump($result2);
+        echo'</pre>';
+
         foreach($result2 as $value){
-            echo $value['nom'].'<br/>'.'<br/>'.'<br/>'.'<br/>';
-            foreach($result as $value){
-                echo '<br />';
-                echo $value['date'];
-                echo '<br />';
-                echo $value['article'];
-                echo '<br />'.'<br />';
-        
+
+            echo '<br/> Catégorie : '.$value['nom'].'<br/>'.'<br/>';
+            foreach($result as $val){
+                if($val['nom'] == $value['nom']){
+                    echo $val['date'].'<br/>';
+                    echo $val['article'].'<br/>';
+                    echo'
+                    <form action = "" method="GET">
+<input type="submit" name="modifier" value="Modifier">
+<input type="submit" name="supprimer" value="Supprimer">
+</form>'.'<br/>'.'<br/>'.'<br/>';
+
+                }
             }
+
         }
 
+        if(isset($_GET['modifier'])){
+            echo'ok';
+        }
+
+         }
 }
+?>
+
+
+
