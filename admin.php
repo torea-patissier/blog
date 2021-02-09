19099c86ee0e40097eb2abd779f299f4902e6907
toreapat <torea.patissier@laplateforme.io>
2021-02-09 10:43:28 +0100

    S2 J2

--- admin.php
+++ admin.php
@@ -21,11 +21,27 @@ $ADMIN = new Admin();
 ?> 
 
 <form action="admin.php" method="GET">
-<input type="submit" name="afficher" value="Afficher les catégories">
+<input type="submit" name="afficher" value="Afficher les articles">
+<input type="submit" name="cacher" value="Cacher les articles">
+<input type="submit" name="ajouter" value="Ajouter une catégorie">
 </form> 
 
 <?php
 if(isset($_GET['afficher'])){
     $ADMIN->AdminGestion();
+    if(isset($_GET['cacher'])){
+        header('location:http://localhost:8888/blog/admin.php');
+    }
+}
+
+if(isset($_GET['ajouter'])){
+    ?>
+            <form action="admin.php" method="POST"><br /><br />
+            <label>Catégorie</label><br /><br />
+            <input type="text" name="nom"><br /><br />
+            <input type="submit" name="envoyer" value="Envoyer">
+        </form>
+    <?php
+    $ADMIN->AddCategorie();
 }
 ?>
--- classes/admin.class.php
+++ classes/admin.class.php
@@ -1,7 +1,8 @@
 <?php
 require_once('bdd.php');
 
-class Admin extends bdd{
+class Admin extends bdd
+{
     //Function pour l'admin
     public function AdminGestion()
     {
@@ -13,28 +14,38 @@ class Admin extends bdd{
         $result = $stmt->fetchAll();
         $result2 = $stmt2->fetchAll();
 
-        echo'<pre>';
+        echo '<pre>';
         // var_dump($result);
-        echo'</pre>';
+        echo '</pre>';
 
-        foreach($result2 as $value){
+        foreach ($result2 as $value) {
 
-            echo '<br/> Catégorie : '.$value['nom'].'<br/>'.'<br/>';
-            foreach($result as $val){
-                if($val['nom'] == $value['nom']){
-                    echo $val['date'].'<br/>';
-                    echo $val['article'].'<br/>';
-                    echo $val[0].'<br/>';
+            echo '<br/> Catégorie : ' . $value['nom'] . '<br/>' . '<br/>';
+            foreach ($result as $val) {
+                if ($val['nom'] == $value['nom']) {
+                    echo $val['date'] . '<br/>';
+                    echo $val['article'] . '<br/>';
+                    echo $val[0] . '<br/>';
                     $idget = $val[0];
-                    echo'
+                    echo '
                     <li>
-                    <a href="modifier_article.php?id='.$idget.'">Modifier |</a>
-                    <a href="supprimer.php?id='.$idget.'">Supprimer</a>
+                    <a href="modifier_article.php?id=' . $idget . '">Modifier |</a>
+                    <a href="supprimer.php?id=' . $idget . '">Supprimer</a>
                     </li><br/><br/>';
-
                 }
-            } 
+            }
         }
     }
-} 
+
+    public function AddCategorie()
+    {
+ if (isset($_POST['envoyer']) && !empty($_POST['nom'])) {
+            $nom = htmlspecialchars($_POST['nom']);
+            $con = $this->connectDb();
+            $stmt = $con->prepare(" INSERT INTO categories (`nom`) VALUES ('$nom') ");
+            $stmt->execute();
+            header('location:http://localhost:8888/blog/admin.php');
+        }
+    }
+}
 ?>
